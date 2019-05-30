b9190c86ac2df4239efca94a0a3b349002991017
yiqiaoxihui <937285794@qq.com>
2019-05-30 12:55:05 -0400

    asdf

--- auto_test_packet_type.py
+++ auto_test_packet_type.py
@@ -18,19 +18,17 @@ packet_type_dic[4]='MIX'
 for key in packet_type_dic:
 	print packet_type_dic[key]
 	time.sleep(3)
-	fw=open("fi36.56.0.0_13.min26.verbose."+packet_type_dic[key],"w")
-	script_args="'max_timeout_per_hop=3,packet_type="+packet_type_dic[key]+",verbose=1,ip=36.56.0.0/20,output_type=file,output_filename=fi36.56.0.0_13.min26."+packet_type_dic[key]+",improve=1'"
+	fw=open("fi36.56.0.0_20.min24.verbose."+packet_type_dic[key],"w")
+	script_args="'max_timeout_per_hop=3,packet_type="+packet_type_dic[key]+",max_prefix_len=26,min_prefix_len=24,min_no_new_prefix=26,verbose=1,ip=36.56.0.0/20,output_type=file,output_filename=fi36.56.0.0_20.min24."+packet_type_dic[key]+"'"
 	cmd="nmap -e "+iface+" --script "+script_path+" --script-args="+script_args
 	print cmd
 	fw.write(cmd)
 	cmd = shlex.split(cmd)
 	p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	difference={}
-	set_ttl_and_send=0
 	while p.poll() is None:
-		line = p.stdout.readline().strip()
-		fw.write(line)
-		print line
+		line = p.stdout.readline()
+		fw.write(line.strip()+"\n")
+		print line.strip()+"\n"
 	fw.close()
 	if p.returncode == 0:
 		print('Subprogram success')
