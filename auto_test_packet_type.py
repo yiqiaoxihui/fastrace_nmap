eacb07a6068b8a3d2abddbe892e651a4299e34d7
yiqiaoxihui <937285794@qq.com>
2019-05-29 22:36:51 -0400

    d

--- auto_test_packet_type.py
+++ auto_test_packet_type.py
@@ -10,29 +10,30 @@ ip=sys.argv[4]
 iface=sys.argv[3]
 print script_path, file_path
 packet_type_dic={}
-packet_type_dic[1]='MIX'
+packet_type_dic[1]='TCP'
 packet_type_dic[2]='UDP'
 packet_type_dic[3]='ICMP'
-packet_type_dic[4]='TCP'
+packet_type_dic[4]='MIX'
 
 for key in packet_type_dic:
 	print packet_type_dic[key]
 	time.sleep(3)
 	fw=open("fi36.56.0.0_13.min26.verbose."+packet_type_dic[key],"w")
-	script_args="packet_type="+str(i)+",verbose=1,ip=36.56.0.0/20,output_type=file,output_filename=fi36.56.0.0_13.min20."+packet_type_dic[key]+",improve=1"
+	script_args="'max_timeout_per_hop=3,packet_type="+packet_type_dic[key]+",verbose=1,ip=36.56.0.0/20,output_type=file,output_filename=fi36.56.0.0_13.min26."+packet_type_dic[key]+",improve=1'"
 	cmd="nmap -e "+iface+" --script "+script_path+" --script-args="+script_args
+	print cmd
+	fw.write(cmd)
 	cmd = shlex.split(cmd)
 	p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	difference={}
 	set_ttl_and_send=0
 	while p.poll() is None:
-		line = p.stdout.readline()
+		line = p.stdout.readline().strip()
 		fw.write(line)
-		line = line.strip()
+		print line
 	fw.close()
 	if p.returncode == 0:
 		print('Subprogram success')
 	else:
 		print('Subprogram failed')
-		print i
 		break
